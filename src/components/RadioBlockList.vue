f427fa04706db3ed3c27e74f33c2ab4621c0c8c1
佐藤 雄大 <sato@tryit.co.jp>
2019-05-15 12:09:58 +0900

    RadioBlockList修正

--- src/components/RadioBlockList.vue
+++ src/components/RadioBlockList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="radio-block-list">
     <template v-for="(label, index) in labels">
-      <input type="radio" :name="name" :id="name+'_'+index" :value="index" :checked="index===value">
+      <input type="radio" :name="name" :id="name+'_'+index" :value="index" :checked="String(index)===value">
       <label :for="name+'_'+index"><span>{{index+1}}</span><span>{{label}}</span></label>
     </template>
   </div>
