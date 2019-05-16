99d54624385686263f637ff605a46bf79ac2b2f8
佐藤 雄大 <sato@tryit.co.jp>
2019-05-16 13:07:32 +0900

    グラフのタイトルが表示されていなかったので修正

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -2,34 +2,10 @@
   import {HorizontalBar, mixins} from 'vue-chartjs'
   export default {
     extends: HorizontalBar,
-    props: ['chartData'],
+    props: ['chartData', 'title'],
     mixins: [mixins.reactiveProp],
     data () {
       return {
-        // datacollection: {
-        //   labels: this.props.answerList,
-        //   datasets: [
-        //     {
-        //       label: 'あなたの回答',
-        //       data: this.props.valueList.map((v,i)=>(i!==this.props.selected-1)?0:v),
-        //       sum: this.props.sumList,
-        //       backgroundColor: 'rgba(86, 206, 255, 0.2)',
-        //       borderColor: 'rgba(86, 206, 255, 1)',
-        //       borderWidth: 2,
-        //       stack: 'Stack 1'
-        //     },{
-        //       label: 'この会場',
-        //       data: this.props.valueList.map((v,i)=>(i===this.props.selected-1)?0:v),
-        //       sum: this.props.sumList,
-        //       backgroundColor: 'rgba(255, 206, 86, 0.2)',
-        //       borderColor: 'rgba(255, 206, 86, 1)',
-        //       borderWidth: 2,
-        //       stack: 'Stack 1'
-        //     }
-        //   ],
-        //   selectedId: this.props.selected-1 // 「あなたの回答」判別のために加えた独自のプロパティ
-        // },
-
         options: {
           layout: {
             padding: {
@@ -68,7 +44,7 @@
           title: {
             display: true,
             fontSize: 24,
-            text: this.chartData.questionStr
+            text: this.title
           },
           maintainAspectRatio:false
         },
--- src/views/Questions1A.vue
+++ src/views/Questions1A.vue
@@ -4,7 +4,7 @@
       <template v-slot:left><span style="font-size:1.4em">Q</span>uestion<span  style="font-size:2.0em">{{question.QUESTION_NO}}</span></template>
       {{question.QUESTION_STR}}
     </page-title>
-    <bar-chart :width="824" :height="400" :chart-data="chartData" style="backgroundColor:#fff;width:824px;margin:0 auto;"></bar-chart>
+    <bar-chart :width="824" :height="400" :chart-data="chartData" :title="dataObj.questionStr" style="backgroundColor:#fff;width:824px;margin:0 auto;"></bar-chart>
     <div style="text-align:right;">
       <base-button text="前へ" @click="prevPage" />
       <base-button text="回答" @click="nextPage" />
