1901be5640dc3b80b347c983a554e718388420a6
田口敦 <taguchi@tryit.co.jp>
2019-05-20 14:01:57 +0900

    試乗１作成中

--- src/views/Reporting1Q.vue
+++ src/views/Reporting1Q.vue
@@ -1,39 +1,21 @@
 <template>
-  <div class="question-layout">
+  <div class="question-layout" v-if="questionViewFlg">
     <page-title>
-      <template v-slot:left>試乗レポート<span style="font-size:2.0em">1</span></template>
-      新旧比較 現行 vs 新型
+      <template v-slot:left><span v-html="questionHtml"></span></template>
+      {{questionExplanation}}
     </page-title>
     <div style="padding-left:100px;">
       <p class="bulb">あなたの回答を選択してください</p>
-      <question-title>
-        <template v-slot:left>Question<span style="font-size:2em">1</span></template>
-        0～80km/h加速
-      </question-title>
-      <reporting-radio-header />
-      <reporting-radio name="Q_4_17_0" label="現行" />
-      <reporting-radio name="Q_4_17_1" label="新型" />
-      <question-title>
-        <template v-slot:left>Question<span style="font-size:2em">2</span></template>
-        操縦安定性
-      </question-title>
-      <reporting-radio-header />
-      <reporting-radio name="Q_4_18_0" label="現行" />
-      <reporting-radio name="Q_4_18_1" label="新型" />
-      <question-title>
-        <template v-slot:left>Question<span style="font-size:2em">3</span></template>
-        乗り心地
-      </question-title>
-      <reporting-radio-header />
-      <reporting-radio name="Q_4_19_0" label="現行" />
-      <reporting-radio name="Q_4_19_1" label="新型" />
-      <question-title>
-        <template v-slot:left>Question<span style="font-size:2em">4</span></template>
-        静粛性
-      </question-title>
-      <reporting-radio-header />
-      <reporting-radio name="Q_4_36_0" label="現行" />
-      <reporting-radio name="Q_4_36_1" label="新型" />
+      <div v-for="question in questionList" :key="'reporting_1'+question.QUESTION_NO">
+        <question-title>
+          <template v-slot:left>Question<span style="font-size:2em">{{question.QUESTION_NO}}</span></template>
+          {{question.QUESTION_STR}}
+        </question-title>
+        <reporting-radio-header />
+        <reporting-radio :name="'Q_'+question.QUESTION_NO+'_0'" label="現行" />
+        <reporting-radio :name="'Q_'+question.QUESTION_NO+'_1'" label="新型" />
+      </div>
+
     </div>
     <h2>フリーコメント（120文字まで）</h2>
     <textarea-balloon name="freeComment" id="freeComment" value=""></textarea-balloon>
@@ -43,6 +25,83 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+	// データ定義
+	data: function(){
+		return {
+			pageType: 'reporting_1',
+			questionNo: 1,
+			questionList: [],
+			questionName: "",
+			questionHtml: "",
+			questionViewFlg: false, // データセット後に描画を行う
+		}
+	},
+	// 初回処理（createdではDOM操作をしない）
+	created: function () {
+		console.log('-- '+this.pageType+'_q');
+		// セッション情報の取得等
+		this.isLogin();
+		this.startSession(this.callback_getSession);
+	},
+	// メソッド群
+	methods: {
+		// バリデーション
+		validation: function () {
+			for( var no in this.questionList ) {
+				if( this.questionList[no].selectedNo == null ) {
+					alert("回答を選択して下さい。");
+					return false;
+				}
+			}
+			return true;
+		},
+		// 前ページへ
+		prevPage: function(e){
+			this.questionNo--;
+			this.$parent.session.question_atr[this.pageType].currentQuestionNo--;
+			this.jump({ name: this.pageType+'_a' });
+		},
+		// 回答
+		nextPage: function(e){
+			if( this.validation() ) {
+				var form = [];
+				for( var no in this.questionList ) {
+					form.push(this.questionList[no]);
+				}
+				console.log("memberId : "+this.getMemberId());
+				this.submit(process.env.VUE_APP_API_URL_BASE+'/'+this.pageType + '/' + this.getMemberId(),form,this.collback_postData);
+			}
+		},
+		// -- サーバサイドからのコールバック
+		// セッション読み込み後
+		callback_getSession: function() {
+			// セッションを読み込み終わって状態を取得したら問題データを読み込む
+			this.questionNo = this.$parent.session.question_atr[this.pageType].currentQuestionNo;
+			this.getJson(process.env.VUE_APP_API_URL_BASE+'/'+this.pageType + '_q/' + this.getMemberId(),this.collback_getData);
+			this.questionName = this.$parent.session.question_atr[this.pageType].QUESTION_NAME;
+			this.questionHtml = this.$parent.session.question_atr[this.pageType].QUESTION_HTML;
+			this.questionExplanation = this.$parent.session.question_atr[this.pageType].QUESTION_EXPLANATION;
+		},
+		// 問題データ取得後
+		collback_getData: function(response) {
+			this.questionList = response.data;
+			this.questionViewFlg = true;
+		},
+		// 回答データ送信後
+		collback_postData: function(response) {
+			this.result = response.data;
+			if( this.result != null ) {
+				this.jump({ name: this.pageType+'_a' });
+			} else {
+				alert("通信が正常に完了しませんでした。電波の良いところで再度お試し下さい。");
+			}
+		}
+	}
+}
+</script>
 
 <style lang="scss">
 .bulb{
@@ -124,11 +183,3 @@
 
 </style>
 
-<script>
-export default {
-  data: function(){
-    return {
-    }
-  }
-}
-</script>
