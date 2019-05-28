dffb8b516102ac918e21727f1c01f5f326097b85
田口敦 <taguchi@tryit.co.jp>
2019-05-28 16:33:08 +0900

    Config仮アップ

---
+++ src/components/ConfigRadio.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{title}}
+    <input type="radio" :id="type+'_on'" :name="name" value="1" :checked="1===value" @change="$emit('input', Number($event.target.value))" v-model="value">
+    <label for="type+'_on'">ON</label>
+    <input type="radio" :id="type+'off'" :name="name" value="0" :checked="0===value" @change="$emit('input', Number($event.target.value))" v-model="value">
+    <label for="type+'_on'">OFF</label>
+<!--
+      <input
+        type="radio"
+        :name="name"
+        :id="name+'_'+index"
+        :value="index"
+        :checked="index===value"
+        @change="$emit('input', Number($event.target.value))"
+      >
+      <label :for="name+'_'+index">
+        <span>{{index+1}}</span>
+        <span>{{label}}</span>
+      </label>
+-->
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: String,
+    type: String,
+    name: String,
+    value: Number
+  }
+}
+</script>
--- src/views/Config.vue
+++ src/views/Config.vue
@@ -6,6 +6,18 @@
       </div>
       <br>
       <form @submit.prevent="submit">
+        <h2>有効設定</h2>
+<!--
+        {{statusHome.enableList['questions_1'].name}}
+        <input type="radio" id="questions_1_on" value="1" v-model="statusHome.enableList['questions_1'].value">
+        <label for="questions_1_on">ON</label>
+        <input type="radio" id="questions_1_off" value="0" v-model="statusHome.enableList['questions_1'].value">
+        <label for="questions_1_off">OFF</label>
+        <br>
+-->
+        <div v-for="questionType in questionList">
+          <config-radio :name="questionType" :type="questionType" :title="statusHome.enableList[questionType].name"  v-model="statusHome.enableList[questionType].value" />
+        </div>
 
 
         <p>{{errorMessage}}</p>
@@ -29,6 +41,21 @@ export default {
 		return {
 			errorMessage: '',
 			configData: {},
+			statusHome: {},
+			// 並べる順番
+			questionList: [
+				'questions_1',      // オリエンテーション
+				'reporting_1',      // 試乗①
+				'questions_2',      // 座学
+				'reporting_2',      // 試乗②
+				'reporting_3',      // 現車・競合車確認
+				'questions_3',      // まとめ①
+				'questions_4',      // まとめ②
+				'catchphrase',      // キャッチフレーズ
+				'faq',              // 勉強会Q&A
+				'examinations_1',   // 理解度確認テスト
+				'enquetes_1',       // アンケート
+			],
 			pageViewFlg: false, // データセット後に描画を行う
 		}
 	},
@@ -41,14 +68,13 @@ export default {
 	// メソッド群
 	methods: {
 		// バリデーション
-		validation: function () {
+		validation: function (callback) {
+			callback();
 			return true;
 		},
 		// Submit
 		submit: function () {
-			if( this.validation() ) {
-				this.$router.push("/"); // ホームに遷移
-			}
+			this.validation(this.callback_formSubmit);
 		},
 		// -- サーバサイドからのコールバック
 		// セッション読み込み後
@@ -60,8 +86,21 @@ export default {
 		// Configデータ取得後
 		collback_getData: function(response) {
 			this.configData = response.data;
-			console.log(this.configData);
+			this.statusHome = this.configData.statusHome;
+			console.log(this.statusHome.enableList['questions_1'].name);
 			this.pageViewFlg = true;
+		},
+		// フォームのSubmit
+		callback_formSubmit: function(e) {
+			var enableList = {};
+			for( var no in this.questionList ) {
+				var questionType = this.questionList[no];
+				enableList[questionType] = this.statusHome.enableList[questionType].value;
+			}
+			var form = {};
+			form.enableList = enableList;
+			console.log(form.enableList);
+			//this.submit(this.getAPIPath()+'/'+this.pageType + '/' + this.getMemberId(),form,this.collback_postData);
 		},
 	}
 }
