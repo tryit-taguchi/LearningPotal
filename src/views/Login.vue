d6cb2a0926aa3c5da6a4ae1b99892baa92733fb3
田口敦 <taguchi@tryit.co.jp>
2019-05-31 19:47:22 +0900

    ログインをソフトキーボード対応にした

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,98 @@
 <template>
-  <form @submit.prevent="save">
-    <div>
-      ログイン画面（席番号入力）
-      <input type="text" v-model="SEAT_CD" placeholder="座席番号">
-      <input type="submit" value="Login">
+    <div style="text-align:center">
+      <page-title>
+        ログイン（席番号入力）
+      </page-title>
+      <input type="text" v-model="SEAT_CD_1" placeholder="" maxlength="1" style="font-size:70pt;width:70pt;text-align:center;">
+      <span style="font-size:70pt">－</span>
+      <input type="text" v-model="SEAT_CD_2" placeholder="" maxlength="1" style="font-size:70pt;width:70pt;text-align:center;">
+      <input type="text" v-model="SEAT_CD_3" placeholder="" maxlength="1" style="font-size:70pt;width:70pt;text-align:center;">
+      <br>
+      <table align="center">
+        <tr>
+          <td>
+            <table class="softkey">
+              <tr>
+                <td @click="softkeyA($event,'A')">A</td>
+                <td @click="softkeyA($event,'B')">B</td>
+                <td @click="softkeyA($event,'C')">C</td>
+                <td @click="softkeyA($event,'D')">D</td>
+                <td @click="softkeyA($event,'E')">E</td>
+                <td @click="softkeyA($event,'F')">F</td>
+                <td @click="softkeyA($event,'G')">G</td>
+              </tr>
+              <tr>
+                <td @click="softkeyA($event,'H')">H</td>
+                <td @click="softkeyA($event,'I')">I</td>
+                <td @click="softkeyA($event,'J')">J</td>
+                <td @click="softkeyA($event,'K')">K</td>
+                <td @click="softkeyA($event,'L')">L</td>
+                <td @click="softkeyA($event,'M')">M</td>
+                <td @click="softkeyA($event,'N')">N</td>
+              </tr>
+              <tr>
+                <td @click="softkeyA($event,'O')">O</td>
+                <td @click="softkeyA($event,'P')">P</td>
+                <td @click="softkeyA($event,'Q')">Q</td>
+                <td @click="softkeyA($event,'R')">R</td>
+                <td @click="softkeyA($event,'S')">S</td>
+                <td @click="softkeyA($event,'T')">T</td>
+                <td @click="softkeyA($event,'U')">U</td>
+              </tr>
+              <tr>
+                <td @click="softkeyA($event,'V')">V</td>
+                <td @click="softkeyA($event,'W')">W</td>
+                <td @click="softkeyA($event,'X')">X</td>
+                <td @click="softkeyA($event,'Y')">Y</td>
+                <td @click="softkeyA($event,'Z')">Z</td>
+                <td></td>
+                <td></td>
+              </tr>
+            </table>
+          </td>
+          <td style="width:30px">
+          </td>
+          <td>
+            <table class="softkey">
+              <tr>
+                <td @click="softkeyN($event,'7')">7</td>
+                <td @click="softkeyN($event,'8')">8</td>
+                <td @click="softkeyN($event,'9')">9</td>
+              </tr>
+              <tr>
+                <td @click="softkeyN($event,'4')">4</td>
+                <td @click="softkeyN($event,'5')">5</td>
+                <td @click="softkeyN($event,'6')">6</td>
+              </tr>
+              <tr>
+                <td @click="softkeyN($event,'1')">1</td>
+                <td @click="softkeyN($event,'2')">2</td>
+                <td @click="softkeyN($event,'3')">3</td>
+              </tr>
+              <tr>
+                <td @click="softkeyN($event,'0')">0</td>
+                <td colspan="2" @click="softkeyC($event)">Clear</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      <base-button text="ログイン" @click="save" />
       <p>{{errorMessage}}</p>
     </div>
-  </form>
 </template>
 
 <style lang="scss">
-  
+table.softkey {
+  border-collapse:collapse;
+  border:0px;
+}
+table.softkey tr td {
+  padding:2px;
+  font-size:24pt;
+  width: 60pt;
+  border:1px solid #fff;
+}
 </style>
 
 <script>
@@ -19,11 +101,19 @@ export default {
 	data: function(){
 		return {
 			errorMessage: '',
-			SEAT_CD: process.env.VUE_APP_DEFAULT_SEAT_CD
+			SEAT_CD: process.env.VUE_APP_DEFAULT_SEAT_CD,
+			SEAT_CD_1: '',
+			SEAT_CD_2: '',
+			SEAT_CD_3: '',
+			oldColor: '#000',
+			blinkColor: '#88c',
 		}
 	},
 	// 初回処理
-	mounted: function () {
+	created: function () {
+		this.SEAT_CD_1 = this.SEAT_CD.charAt(0);
+		this.SEAT_CD_2 = this.SEAT_CD.charAt(1);
+		this.SEAT_CD_3 = this.SEAT_CD.charAt(2);
 		console.log("ログアウト処理");
 		this.toLogout();
 	},
@@ -31,12 +121,48 @@ export default {
 	methods: {
 		// バリデーション
 		validation: function () {
-			if( this.SEAT_CD == "" ) {
+			this.SEAT_CD = this.SEAT_CD_1+this.SEAT_CD_2+this.SEAT_CD_3;
+
+			if( this.SEAT_CD_1 == ''
+			 || this.SEAT_CD_2 == ''
+			 || this.SEAT_CD_3 == '' ) {
 				this.errorMessage = "座席番号を入力してください。";
 				return false;
 			}
 			return true;
 		},
+		// softkey アルファベット
+		softkeyA: function (event,val) {
+			console.log(val);
+			this.SEAT_CD_1 = val;
+			this.softkeyClickColor(event); // 色を変える
+		},
+		// softkey 数字
+		softkeyN: function (event,val) {
+			console.log(val);
+			if( this.SEAT_CD_2 == '' ) {
+				this.SEAT_CD_2 = val;
+			} else if( this.SEAT_CD_3 == '' ) {
+				this.SEAT_CD_3 = val;
+			}
+			this.softkeyClickColor(event); // 色を変える
+		},
+		// softkey クリア
+		softkeyC: function (event) {
+			console.log("Clear");
+			this.SEAT_CD_1 = '';
+			this.SEAT_CD_2 = '';
+			this.SEAT_CD_3 = '';
+			this.softkeyClickColor(event); // 色を変える
+		},
+		// softkey クリックカラー処理
+		softkeyClickColor: function (event) {
+			event.target.style.background = this.blinkColor;
+			setTimeout(() => {
+				event.target.style.background = this.oldColor;
+			}, 200);
+		},
+
 		// 保存
 		save: function () {
 			if( this.validation() ) {
