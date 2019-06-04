f98d793909857d5169c75a0b666ef4c1812a763b
田口敦 <taguchi@tryit.co.jp>
2019-06-04 11:32:09 +0900

    ホーム画面追加時にフルスクリーンに設定

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/nlp/' : ''
+  publicPath: process.env.NODE_ENV === 'production' ? '/nlp/' : '',
+  pwa: {
+      themeColor: '#4DBA87', // The Vue color
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'default',
+  }
 }
