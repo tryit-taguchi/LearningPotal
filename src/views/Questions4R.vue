32a4fb3d7357d43178fd75b9969cccab82a3922c
田口敦 <taguchi@tryit.co.jp>
2019-06-06 10:10:49 +0900

    まとめ２の結果を固定データからロードデータに変更とりあえず版

--- src/views/Questions4R.vue
+++ src/views/Questions4R.vue
@@ -1,72 +1,94 @@
 <template>
   <div>
-    <page-title before-text="まとめ② 回答一覧（棒グラフ）（みんなの声用）">
+    <template v-for="question in questionList">
+      <page-title :before-text="questionName">
+        <template v-slot:left><span style="font-size:1.4em">Q</span>uestion<span  style="font-size:2.0em">{{question.QUESTION_NO}}</span></template>
+        {{question.QUESTION_STR}}
+      </page-title>
+      <bar-chart-result v-if="chartViewFlg" :width="824" :height="400" :chart-data="question" chart-index="0" show-your-select />
+    </template>
+  </div>
+</template>
+<!--
+<template>
+  <div>
+    <page-title before-text="questionName">
       <template v-slot:left><span style="font-size:1.4em">Q</span>uestion<span  style="font-size:2.0em">{{current+1}}</span></template>
       {{questions[current].text}}
     </page-title>
-    <bar-chart-result v-if="chartViewFlg" :width="824" :height="450" :chart-data="question" chart-index="0" show-your-select />
+    <bar-chart-result v-if="chartViewFlg" :width="824" :height="450" :chart-data="question" show-your-select />
     <button-area>
       <base-button text="前へ" @click="prevQuestion" v-if="current>0" />
       <base-button text="回答" @click="nextQuestion" />
     </button-area>
   </div>
 </template>
+-->
 
 <script>
 export default {
-  data: function () {
-    return {
-      current: 0,
-      questions: [{}],
-      userSelects: [null, null, null, null],
-      chartViewFlg: true,
-      question: {
-        "QUESTION_ID":"10",
-        "QUESTION_STR":"新型デイズの「売り・気に入った点」は？（３つ答えて）",
-        "QUESTION_NO":"1",
-        "QUESTION_CNT":"1",
-        "ANSWER_CNT":"10",
-        "answerList":["走り","デザイン（内外装）","広さ（居室・荷室）","収納","カラー","シート","安全装備","プロパイロット","女性に優しい機能・装備","その他"],
-        "selectedNo":8,
-        "selectedNoList":[4,6,8],
-        "siteValueList":[10,8,15,7,15,6,12,9,12,5],
-        "siteSumList":["10","8","15","7","15","6","12","9","12","5"],
-        "totalValueList":[6,12,11,6,20,5,10,8,9,12],
-        "totalSumList":["6","12","11","6","20","5","10","8","9","12"],
-        "questionStr":"Q1. 新型デイズの「売り・気に入った点」は？（３つ答えて）"
-      }
-    }
-  },
-  methods: {
-    prevQuestion: function(e){
-      this.current--;
-    },
-    nextQuestion: function(e){
-      if(this.userSelects[this.current]!==null){
-        if(this.current>=3){
-          this.$router.push('sensebefore1result');
-        }else{
-          this.current++;
-        }
-      }else{
-        alert('回答を選択してください');
-      }
-    },
-    collback_QuestionsLoad: function(response) {
-			this.questions = response.data;
-    },
-    collback_AnswerPost: function(response) {
-			console.log(response.data);
-    }
-  },
-	mounted: function () {
-		//this.$cookies.set('loginId', '124');
-		// console.log(this.$cookies.get('loginId'));
-		// console.log(this.$cookies.get('PHPSESSID'));
-		// console.log(this.$cookies.get('LOGIN_DATE'));
-		//Cookies.set('name','value', { expires: 0.5 });
-		this.getJson(this.getAPIPath()+'/questions_1',this.collback_QuestionsLoad);
-		this.postJson(this.getAPIPath()+'/questions_1',this.collback_AnswerPost);
+	// データ定義
+	data: function(){
+		return {
+			pageType: 'questions_4',
+			questionNo: 1,
+			questionList: [],
+			questionName: "",
+			chartViewFlg: false, // データセット後に描画を行う
+		}
+	},
+	// 初回処理（createdではDOM操作をしない）
+	created: function () {
+		console.log('-- '+this.pageType+'_a');
+		// セッション情報の取得等
+		this.isLogin(); // ログインチェック・ログインしていたらセッション取得
+		this.startSession(this.callback_getSession);
+	},
+	// メソッド群
+	methods: {
+		// バリデーション
+		validation: function () {
+			return true;
+		},
+		// 前ページへ
+		prevPage: function(e){
+			this.questionNo--;
+			this.jump({ name: this.pageType+'_q' });
+		},
+		// 次の質問へ
+		nextPage: function(e){
+			this.$parent.session.question_atr[this.pageType].currentQuestionNo++;
+			if( this.$parent.session.question_atr[this.pageType].currentQuestionNo < this.$parent.session.question_atr[this.pageType].QUESTION_CNT ) {
+				this.jump({ name: this.pageType+'_q' });
+			} else {
+				this.$parent.session.question_atr[this.pageType].QUESTION_COMPLETE = true;
+				this.jump({ name: this.pageType+'_r' });
+			}
+		},
+		// -- サーバサイドからのコールバック
+		// セッション読み込み後
+		callback_getSession: function() {
+			// セッションを読み込み終わって状態を取得したら問題データを読み込む
+			this.questionNo = this.$parent.session.question_atr[this.pageType].currentQuestionNo;
+			this.getJson(this.getAPIPath()+'/'+this.pageType + '_a/' + this.getMemberId() + '/' + this.questionNo,this.collback_getData);
+			this.questionName = this.$parent.session.question_atr[this.pageType].QUESTION_NAME;
+		},
+		// 問題データ取得後
+		collback_getData: function(response) {
+			this.questionList = response.data.questionList;
+			for( var no in this.questionList ) {
+				var question = this.questionList[no];
+				// グラフ用のタイトル
+				question.questionStr = 'Q' + this.questionList[no].QUESTION_NO + '. ' + this.questionList[no].QUESTION_STR;
+				//question.answerList = ["①100台","②1～5台","③6～9台","④10台以上"];
+				//question.valueList = [75,75,0,0];
+				//question.sumList = [3,3,0,0];
+				//question.selectedNo = null;
+			}
+
+			this.chartViewFlg = true;
+			this.$forceUpdate();
+		},
 	}
 }
 </script>
