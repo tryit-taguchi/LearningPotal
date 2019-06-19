c9969928bff566d4127bddc6be3e80e47c6dd5fc
田口敦 <taguchi@tryit.co.jp>
2019-06-19 17:16:27 +0900

    印刷用データ配置（仮）

--- src/views/Print.vue
+++ src/views/Print.vue
@@ -1,21 +1,65 @@
 <template>
-	<div>
-    <page-title>
-      <template v-slot:left>比較</template>
-      000
-    </page-title>
-		<div v-for="(sub,no) in clist">
-			<div>
-				<div style="font-size:150%" :id="'chart'+no">　{{sub.questionName}}</div>
-				<div class="graph" v-if="sub.chartViewFlg">
-					<radar-chart-reporting :width="412" :height="450" :chart-data="sub.chartData1" />
-					<radar-chart-reporting :width="412" :height="450" :chart-data="sub.chartData2" />
+	<!-- データの読み込みが終わったらレンダリング -->
+	<div v-if="chartViewFlg">
+		<!-- 該当受講者分ループを回す 1受講者につき2枚 -->
+		<div v-for="(sub,no) in this.answers">
+			<section class="print_pages">
+				<div>
+					試乗付き勉強会レポート
 				</div>
-	      <button-area>
-	        <base-button text="次へ" v-if="no<clist.length-1" v-scroll-to="'#chart'+(parseInt(no)+1)" />
-	      </button-area>
-	    </div>
+				<!-- 受講者名 -->
+				<div>
+				{{sub.member.COMPANY_NAME}} {{sub.member.MEMBER_NAME}}
+				</div>
+				<!-- レーダーチャート -->
+				<div v-for="(raderChart, questionType, index) in sub.raderChartList">
+					{{raderChart.questionName}}
+					<div class="graph">
+						<radar-chart-reporting :width="412" :height="450" :chart-data="raderChart.member" />
+						<radar-chart-reporting :width="412" :height="450" :chart-data="raderChart.site" />
+					</div>
+				</div>
+				<!-- 比較グラフ（まとめ①） -->
+				{{sub.compareBarChartName}}
+				<div v-for="(compareChart, questionNo, index) in sub.compareBarChartList">
+					<div>{{compareChart.title}}</div>
+					<div class="graph">
+						<bar-chart :width="412" :height="400" :chart-options="compareChart.before" />
+						<bar-chart :width="412" :height="400" :chart-options="compareChart.after" />
+					</div>
+				</div>
+				<!-- 複数選択グラフ（まとめ②） -->
+				{{sub.multipleBarChartName}}
+				<div v-for="(multipleChart, questionNo, index) in sub.multipleBarChartList">
+					<div>{{multipleChart.title}}</div>
+					<div class="graph">
+						<bar-chart :width="824" :height="450" :chart-options="multipleChart.chart" />
+					</div>
+				</div>
+				<div>
+					キャッチフレーズ<!-- 1枚目のBest3 -->
+				</div>
+				<div v-for="(phrase, index) in catchphrase.catchphraseRankList">
+					<div>
+						Best {{phrase.RANK}} {{phrase.CATCHPHRASE_STR}} 
+					</div>
+				</div>
+				<hr><!-- とりあえずページ分けをわかりやすくするためにhr入れてるほんとはいらない -->
+				<div>
+					過去のキャッチフレーズ<!-- 2枚目の前半後半のBest1 3列x9行の27個分 -->
+				</div>
+				<div v-for="(phrase, index) in catchphrase.catchphraseOldList">
+					<div>
+						{{phrase.catchphraseDate}}<br>
+						{{phrase.siteName}}<br>
+						前半 {{phrase.catchphraseStr[1]}}<br>
+						後半 {{phrase.catchphraseStr[2]}}<br>
+					</div>
+				</div>
+				<hr><!-- とりあえずページ分けをわかりやすくするためにhr入れてるほんとはいらない -->
+			</section>
 		</div>
+
 	</div>
 </template>
 
@@ -24,6 +68,10 @@ export default {
 	// データ定義
 	data: function(){
 		return {
+			chartViewFlg : false,
+			catchphrase : {},
+			answers : [],
+/*
 			clist :
 			[
 				{
@@ -120,26 +168,38 @@ export default {
 					chartViewFlg: false, // データセット後に描画を行う
 				},
 			],
+*/
 		}
 	},
 	// 初回処理（createdではDOM操作をしない）
 	created: function () {
 		console.log('-- '+this.pageType+'_a');
 		console.log(this.$route.params);
+		var lectureDate = this.$route.params.lectureDate;
+		var siteName    = this.$route.params.siteName;
+		var lectureType = this.$route.params.lectureType;
+		this.getJson(this.getAPIPath()+'/print/' + lectureDate + '/' + siteName + '/' + lectureType,function(response) {
+			this.catchphrase = response.data.catchphrase; // キャッチフレーズデータ
+			this.answers = response.data.answers; // グラフ用データ（該当受講者分のリスト）
 
-		// セッション情報の取得等
-		this.isLogin(); // ログインチェック・ログインしていたらセッション取得
-		this.startSession(this.callback_getSession);
+//			console.log(response.data.answers.raderChartList);
+
+			this.chartViewFlg = true;
+		}.bind(this));
 	},
 	// メソッド群
 	methods: {
+/*
 		// -- サーバサイドからのコールバック
 		// セッション読み込み後
 		callback_getSession: function() {
 			// セッションを読み込み終わって状態を取得したら回答データを読み込む
+			/print/20190619/追浜グランドライブ/1
+
 			for( var no in this.clist ) {
 				var sub = this.clist[no];
 				var pageType = sub.pageType;
+
 				this.getJson(this.getAPIPath()+'/'+pageType + '_a/' + this.getMemberId(),function(response) {
 					// 問題データ取得後
 					this.questionList = response.data.questionList;
@@ -182,6 +242,7 @@ export default {
 				sub.questionMaxValue = this.$parent.session.question_atr[pageType].ANSWER_SELECT_CNT;
 			}
 		},
+*/
 	},
 }
 </script>
@@ -194,4 +255,17 @@ export default {
 	margin: 10px auto 0;
 	background-color: #fff;
 }
+
+/*A3横*/
+.print_pages{
+	width: 410mm;
+/*	height: 292mm;*/
+	page-break-after: always;
+}
+/*最後のページは改ページを入れない*/
+.print_pages:last-child{
+	page-break-after: auto;
+}
+
+
 </style>
